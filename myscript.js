5250fb7c58d1512860980b41151435651c949681
rkotchamp <106193123+rkotchamp@users.noreply.github.com>
2023-04-21 03:36:54 +0300

    made changes to my whole setup

---
+++ myscript.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const burger = document.querySelector(".burger");
+const icons = document.querySelector(".icons");
+const hireButton = document.querySelector(".hireButton");
+const image = document.querySelector(".pic");
+const logo = document.querySelector(".logo");
+const overlay = document.querySelector(".overlay");
+const secondContainer = document.querySelector(".second-container ");
