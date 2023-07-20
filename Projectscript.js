0ef7de88fdb82ef8c8e7df44c683185730651bd8
Reuben <treuben90@yahoo.com>
2023-07-20 12:02:55 +0300

    added interactivity to the project page with Js and some few other changes

---
+++ Projectscript.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const visitLink = document.querySelectorAll(".visitLink");
+const visible = document.querySelector(".visible");
+const cards = document.querySelectorAll(".cards");
+const SeeMoreButton = document.querySelector(".seeMore");
+const card7 = document.querySelector(".card--7");
+const card8 = document.querySelector(".card--8");
+
+// Mouse hover on cards
+cards.forEach((card, index) => {
+  card.addEventListener("mouseover", function () {
+    visitLink[index].classList.toggle("visible");
+    console.log(card);
+  });
+});
+
+cards.forEach((card, index) => {
+  card.addEventListener("mouseleave", function () {
+    visitLink[index].classList.toggle("visible");
+    console.log(card);
+  });
+});
+// mouse hover on card done
+
+// Hide and display cards when see more button is clicked
+document.addEventListener("DOMContentLoaded", function () {
+  SeeMoreButton.addEventListener("click", function () {
+    card7.style.display = card7.style.display === "none" ? "flex" : "none";
+    card8.style.display = card8.style.display === "none" ? "flex" : "none";
+    if (card7.style.display === "flex" && card8.style.display === "flex") {
+      SeeMoreButton.textContent = "Hide";
+    } else {
+      SeeMoreButton.textContent = "See More";
+    }
+  });
+});
--- script.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const burger = document.querySelector(".burger");
-const icons = document.querySelector(".icons");
-const hireButton = document.querySelector(".hireButton");
-const image = document.querySelector(".pic");
-const logo = document.querySelector(".logo");
-const overlay = document.querySelector(".overlay");
-const secondContainer = document.querySelector(".second-container ");
